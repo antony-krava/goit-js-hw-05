9cccb665a05f439729c31c80ed623b9d56bcc2be
Anton Kravchenko <lugagold@gmail.com>
2020-12-29 15:21:02 +0200

    homework done
    Выполнена домашняя работа

---
+++ js/task-5.js
@@ -0,0 +1,62 @@
+class Car {
+  static getSpecs(car) {
+    console.log(`Max Speed: ${car.maxSpeed}, Speed: ${car.speed}, Is On: ${car.isOn}, Distance: ${car.distance}, Price: ${car._price}`)
+  }
+
+  constructor({ price, maxSpeed }) {
+    this.speed = 0
+    this._price = price
+    this.maxSpeed = maxSpeed
+    this.isOn = false
+    this.distance = 0
+  }
+
+  get price() {
+    return this._price
+  }
+
+  set price(value) {
+    this._price = value
+  }
+
+  turnOn() {
+    this.isOn = true
+  }
+
+  turnOff() {
+    this.isOn = false
+    this.speed = 0
+  }
+
+  accelerate(accelerateValue) {
+    this.speed + accelerateValue <= this.maxSpeed ? (this.speed = this.speed + accelerateValue) : false
+  }
+
+  decelerate(decelerateValue) {
+    this.speed - decelerateValue >= 0 ? (this.speed = this.speed - decelerateValue) : false
+  }
+
+  drive(hours) {
+    this.isOn ? (this.distance = this.distance + hours * this.speed) : false
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 })
+
+mustang.turnOn()
+mustang.accelerate(50)
+mustang.drive(2)
+
+Car.getSpecs(mustang)
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20)
+mustang.drive(1)
+mustang.turnOff()
+
+Car.getSpecs(mustang)
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price) // 2000
+mustang.price = 4000
+console.log(mustang.price) // 4000
